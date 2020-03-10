875d8c0b37d35758304fdcd8a92dadf74edea33a
zengshide <1909493019@qq.com>
2020-03-10 20:45:20 +0800

    add: 增加 Icon 组件 :sparkles:

--- src/components/Demo1.vue
+++ src/components/Demo1.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>Demo1</div>
+  <div class="demo1-wrap">
+    <Icon class="fa-address-book"></Icon>
+  </div>
 </template>
 
 <script>
+import Icon from "./packages/Icon";
 export default {
-  name: 'Demo1'
+  name: "Demo1",
+  data() {
+    return {};
+  },
+  components: {
+    Icon
+  }
 };
 </script>
 
---
+++ src/components/packages/Icon.vue
@@ -0,0 +1,13 @@
+<template>
+  <i class="fa"></i>
+</template>
+
+<script>
+export default {
+  name: "Icon"
+};
+</script>
+
+<style lang="less" scoped>
+@import "../packages/styles/t-font.less";
+</style>
