cbbcdc45a10491a2883d980901811868adf6d68e
zengshide <1909493019@qq.com>
2020-03-12 21:35:39 +0800

    add: provide 和 Inject

--- src/components/Demo2.vue
+++ src/components/Demo2.vue
@@ -1,10 +1,35 @@
 <template>
-  <div>Demo2</div>
+  <div>
+    <p>parent: {{ count }}</p>
+    <p>
+      <SubDemo2 />
+    </p>
+  </div>
 </template>
 
 <script>
+import SubDemo2 from "./subDemo2";
 export default {
-  name: "Demo2"
+  name: "Demo2",
+  data() {
+    return {
+      count: 0
+    };
+  },
+  provide() {
+    return {
+      par: this
+    };
+  },
+  components: {
+    SubDemo2
+  },
+  mounted() {
+    this.timer = setTimeout(() => {
+      clearTimeout(this.timer);
+      this.count++;
+    }, 1500);
+  }
 };
 </script>
 
---
+++ src/components/subDemo2.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>sub:{{ par.count }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "subDemo2",
+  inject: ["par"]
+};
+</script>
+
+<style lang="less" scoped></style>
